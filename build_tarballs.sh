608c418d9c8564500ebbe86dc7f7bbecdca1598e
Jokubas Liutkus <jokubas@fb.com>
2019-01-17 16:16:05 +0000

    Adding github releases to the travis
    Adding support to travis for GitHub releases which will build rpm, deb and tarballs for multiple OSs and architectures on tagged commits.

---
+++ build_tarballs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CPE2CVE=cpe2cve
+CSV2CPE=csv2cpe
+NVDSYNC=nvdsync
+RPM2CPE=rpm2cpe
+NAME=nvdtools
+VERSION=1.0
+
+function build_binaries_and_tars(){
+    GOOS=$1; shift
+    ARCHS=("$@")
+    for GOARCH in ${ARCHS[@]}; do
+        for BINARY in $CPE2CVE $CSV2CPE $NVDSYNC $RPM2CPE; do
+            env GOOS=$GOOS GOARCH=$GOARCH go build -o binaries/$BINARY-$VERSION-$GOOS-$GOARCH ./cmd/$BINARY
+        done
+	tar -zcvf release/$NAME-$VERSION-$GOOS-$GOARCH.tar.gz \
+            binaries/{$CPE2CVE,$CSV2CPE,$NVDSYNC,$RPM2CPE}-$VERSION-$GOOS-$GOARCH
+    done
+}
+
+mkdir -p {binaries,release}
+
+# create tarballs for different architectures
+archs=(arm64 amd64)
+build_binaries_and_tars linux ${archs[@]}
+
+archs=(amd64 arm)
+build_binaries_and_tars freebsd ${archs[@]}
+
+archs=(amd64 386)
+build_binaries_and_tars windows ${archs[@]}
+
+archs=(amd64)
+build_binaries_and_tars darwin ${archs[@]}
+
+# cleanup
+rm -rf binaries
