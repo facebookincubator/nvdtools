77c29cb76f38726c64059e5a75925e39ad4bf49d
Filip Hrenić <hrenic.filip@gmail.com>
2019-08-06 14:45:55 +0100

    [vulndb] Subdivide batches when inserting fails
    When we insert stuff into vendor_data, sometimes a batch can be to big
    and won't insert. So instead, we just subdivide the batches into smaller
    portions (until a batch size is 1) and try to insert those.
    
    go test ./... works. Tested insert, works

--- vulndb/schema.sql
+++ vulndb/schema.sql
@@ -12,6 +12,10 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
+CREATE DATABASE IF NOT EXISTS vulndb;
+
+USE vulndb;
+
 DROP TABLE IF EXISTS
 	`snooze`,
 	`custom_data`,
