042a4c9deef31c9e0aff23286097f4969eff5198
Filip Hrenić <hrenic.filip@gmail.com>
2019-09-26 11:39:25 +0100

    [client][retries] Exponential backoff and retries of all statuses
    go test ./...

--- providers/lib/client/config.go
+++ providers/lib/client/config.go
@@ -18,17 +18,17 @@ import (
 	"flag"
 	"fmt"
 	"regexp"
-	"strconv"
-	"strings"
 	"time"
 )
+
+var userAgentRe = regexp.MustCompile("^[[:ascii:]]+$")
 
 // Config is used to configure a client
 type Config struct {
 	UserAgent         string
 	numRetries        int
 	retryDelay        time.Duration
-	retriable         ints
+	retryPolicy       RetryPolicy
 	requestsPerPeriod int
 	period            time.Duration
 }
@@ -39,7 +39,7 @@ func (conf *Config) AddFlags() {
 	flag.IntVar(&conf.numRetries, "num-retries", 0, "how many times will specified statuses get retried. 0 means no retries")
 	// TODO implement exponential backoff (for some statuses?)
 	flag.DurationVar(&conf.retryDelay, "retry-delay", time.Second, "delay between each retry")
-	flag.Var(&conf.retriable, "retry", "which http statuses to retry. specify multiple by specifying the flag multiple times or using a comma")
+	flag.Var(&conf.retryPolicy, "retry", "which http statuses to retry. empty string means no retries, all means retry all, or provide a comma separated list of status codes")
 	flag.IntVar(&conf.requestsPerPeriod, "requests-per-period", 0, "how many requests per period to make. 0 means no throttling")
 	flag.DurationVar(&conf.period, "period", time.Second, "period in which requests are capped by the requests-per-period flag")
 }
@@ -48,7 +48,7 @@ func (conf *Config) Validate() error {
 	if conf.UserAgent == "" {
 		return fmt.Errorf("need to specify user agent")
 	}
-	if !regexp.MustCompile("^[[:ascii:]]+$").MatchString(conf.UserAgent) {
+	if !userAgentRe.MatchString(conf.UserAgent) {
 		return fmt.Errorf("User-Agent contains non ascii characters")
 	}
 	return nil
@@ -57,7 +57,7 @@ func (conf *Config) Validate() error {
 // Configure configures the given client (add throttling, retries, ...)
 func (conf *Config) Configure(c Client) Client {
 	if conf.numRetries > 0 {
-		c = WithRetries(c, conf.numRetries, conf.retryDelay, conf.retriable...)
+		c = WithRetries(c, conf.numRetries, conf.retryDelay, conf.retryPolicy)
 	}
 	if conf.requestsPerPeriod > 0 {
 		c = WithThrottling(c, conf.period, conf.requestsPerPeriod)
@@ -66,42 +66,4 @@ func (conf *Config) Configure(c Client) Client {
 		c = WithUserAgent(c, conf.UserAgent)
 	}
 	return c
-}
-
-// flag vars
-
-type ints []int
-
-// Set is a part of the flag.Value interface
-func (ii *ints) Set(ss string) error {
-	if ii == nil {
-		ii = new(ints)
-	}
-	parts := strings.Split(ss, ",")
-	if *ii == nil {
-		*ii = make(ints, 0, len(parts))
-	}
-
-	for _, s := range parts {
-		i, err := strconv.Atoi(s)
-		if err != nil {
-			return err
-		}
-		*ii = append(*ii, i)
-	}
-	return nil
-}
-
-func (ii *ints) String() string {
-	if ii == nil || *ii == nil {
-		return ""
-	}
-	var sb strings.Builder
-	for idx, i := range *ii {
-		if idx > 0 {
-			sb.WriteByte(',')
-		}
-		sb.WriteString(strconv.Itoa(i))
-	}
-	return sb.String()
 }
--- providers/lib/client/retry.go
+++ providers/lib/client/retry.go
@@ -19,8 +19,14 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 )
+
+// backoff means that on each retry, we will multiply the delay time by this
+// maybe this could be configurable as well, but meh
+const backoff = 2
 
 // FailedRetries is an error returned when all retries have been exhausted
 type FailedRetries int
@@ -30,38 +36,80 @@ func (fr FailedRetries) Error() string {
 	return fmt.Sprintf("failed to fetch after %d retries", int(fr))
 }
 
+// RetryPolicy is a function which returns true if HTTP status should be retried
+type RetryPolicy func(status int) bool
+
+func (rp *RetryPolicy) String() string { return "" }
+func (rp *RetryPolicy) Set(s string) error {
+	switch s {
+	case "":
+		*rp = RetryNone
+	case "all":
+		*rp = RetryAll
+	default:
+		// treat it as a comma separated list of ints
+		parts := strings.Split(s, ",")
+		statuses := make([]int, len(parts))
+		for _, part := range parts {
+			status, err := strconv.Atoi(part)
+			if err != nil {
+				return err
+			}
+			statuses = append(statuses, status)
+		}
+		*rp = Retry(statuses...)
+	}
+	return nil
+}
+
+// RetryNone is a RetryPolicy which doesn't retry anything
+func RetryNone(_ int) bool { return false }
+
+// RetryAll is a RetryPolicy which retries all http statuses
+func RetryAll(_ int) bool { return true }
+
+// Retry will only retry given statuses
+func Retry(statuses ...int) RetryPolicy {
+	set := make(map[int]bool, len(statuses))
+	for _, s := range statuses {
+		set[s] = true
+	}
+	return func(status int) bool {
+		return set[status]
+	}
+}
+
 // WithRetries will retry all given requests for the specified number of times
 //	- if status is 200, returns
-//	- if status is one of the specified and hasn't been retried the total number of times, retry
+//	- if status is covered by the retry policy and hasn't been retried the total number of times, retry
 //	- otherwise, fail the request
-func WithRetries(c Client, retries int, delay time.Duration, statuses ...int) Client {
-	if retries <= 0 || len(statuses) == 0 {
+func WithRetries(c Client, retries int, delay time.Duration, rp RetryPolicy) Client {
+	if retries <= 0 {
 		// if no retries, return the normal client
-		// if no statuses are retried, do the same
 		return c
 	}
-	retriable := make(map[int]bool, len(statuses))
-	for _, status := range statuses {
-		retriable[status] = true
-	}
-	return &executorClient{c, &retryExecutor{retries, delay, retriable}}
+	return &executorClient{c, &retryExecutor{retries, delay, rp}}
 }
 
 type retryExecutor struct {
-	retries   int
-	delay     time.Duration
-	retriable map[int]bool
+	retries     int
+	delay       time.Duration
+	shouldRetry RetryPolicy
 }
 
 func (c *retryExecutor) execute(f func() (*http.Response, error)) (*http.Response, error) {
+	delay := c.delay
 	for retry := 0; retry <= c.retries; retry++ {
 		resp, err := f()
 		if err != nil {
 			return resp, err
 		}
 
-		switch {
-		default:
+		if resp.StatusCode == http.StatusOK {
+			return resp, nil
+		}
+
+		if !c.shouldRetry(resp.StatusCode) {
 			// unknown status, read the error and return it
 			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(io.LimitReader(resp.Body, 1024*1024))
@@ -69,15 +117,14 @@ func (c *retryExecutor) execute(f func() (*http.Response, error)) (*http.Respons
 				return nil, fmt.Errorf("cannot read http response: %v", err)
 			}
 			return nil, &Err{resp.StatusCode, resp.Status, string(body)}
-		case resp.StatusCode == http.StatusOK:
-			return resp, nil
-		case c.retriable[resp.StatusCode]:
-			// get out of switch, sleep and retry
 		}
 
+		// retry if have more retries left
+
 		if retry != c.retries {
-			time.Sleep(c.delay)
+			time.Sleep(delay)
 		}
+		delay *= backoff
 	}
 	// no more retries left
 	return nil, FailedRetries(c.retries)
